be0fa173e495e374b32886760d37eb8bcba4ebcb
Delbert <delbertdrums5@gmail.com>
2023-12-21 22:09:46 +0100

    Container widget

---
+++ flutter_widgets/lib/container.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class Contanner extends StatelessWidget {
+  const Contanner({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      /// The Wiget of the day(Container)
+      body: Container(
+        height: 300,
+        width: 300,
+        //Interesting decoration property
+        decoration: BoxDecoration(
+            // color: Colors.deepPurple,
+            borderRadius: BorderRadius.circular(20),
+            shape: BoxShape.rectangle,
+            border: Border.all(
+              color: Colors.black,
+              width: 8,
+            )),
+        child: Center(
+          child: Image.asset(
+            "assets/delbert.jpg",
+            fit: BoxFit.cover,
+          ),
+        ),
+      ),
+    );
+  }
+}
